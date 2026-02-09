b886415d1351b9b0bdf5ceaff0762199ccf897d0
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-02-09 22:42:22 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260209/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    int cnt, mcnt;
+    cnt = 0;
+    mcnt = 1;
+    for(int i=1; i<N; i++){
+        if(arr[i-1] == arr[i]){
+            cnt++;
+        }else{
+            mcnt = max(mcnt, cnt);
+            cnt = 1;
+        }
+    }
+
+    cout << mcnt;
+
+    return 0;
+}
