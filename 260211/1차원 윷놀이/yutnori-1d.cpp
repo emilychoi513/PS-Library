08ab24f5c87cfff14a54645826cd6ee213312edb
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-02-11 20:57:13 +0900

    Title: 1차원 윷놀이, Time: 188ms, Memory: 0MB, Status: Passed - Codetree

--- 260211/1차원 윷놀이/yutnori-1d.cpp
+++ 260211/1차원 윷놀이/yutnori-1d.cpp
@@ -5,37 +5,52 @@
 using namespace std;
 
 int n, m, k;
-int mcnt = 0; // output
 int nums[12];
+
 vector<int> horse;
+int mcnt = 0; // output
+
+int Count(){
+    int cnt = 0;
+
+    for(int i=0; i<k; i++){
+        if(horse[i] >= m) cnt++;
+    }
+    return cnt;
+}
 
 void maxScore(int step){ 
-    if(step == n){
-        int cnt = 0;
-        for(int i=0; i<k; i++){
-            if(horse[i] >= m) cnt++;
-        }
-        mcnt = max(mcnt, cnt);
+    int i, j;
+
+    if(Count() == k){
+        mcnt = k;
         return;
     }
 
-    for(int i=0; i<k; i++){
-        if(horse[i] >= m) continue;
+    if(step == n){
+        mcnt = max(mcnt, Count());
+        return;
+    }
 
-        horse[i] += nums[step];
+    for(j=0; j<k; j++){
+        if(horse[j] >= m) continue;
+
+        horse[j] += nums[step];
         maxScore(step + 1);
-        horse[i] -= nums[step];
+        horse[j] -= nums[step];
     }
 }
 
 int main() {
+    int i;
+
     cin >> n >> m >> k;
 
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         cin >> nums[i];
     }
 
-    horse.resize(k, 1); //말은 1에서 출발
+    horse.resize(k+1, 1); //말은 1에서 출발
     maxScore(0);
     
     cout << mcnt;
