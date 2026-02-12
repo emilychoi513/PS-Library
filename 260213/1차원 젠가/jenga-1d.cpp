72053ab7fcfebcf23b50bd59f5f68146d608eaa2
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-02-13 00:56:53 +0900

    Title: 1차원 젠가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260213/1차원 젠가/jenga-1d.cpp
+++ 260213/1차원 젠가/jenga-1d.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int n;
-int blocks[100];
-int temp[100];
-int temp2[100];
+vector<int> blocks(100);
 int s1, e1;
 int s2, e2;
+
+
+int removeJenga(int cnt, int s, int e){
+    vector<int> temp(100, 0);
+    int tcnt = 0;
+
+    for(int i=0; i<cnt; i++){
+        if(i >= s-1 && i <= e-1) continue;
+
+        temp[tcnt] = blocks[i];
+        tcnt++;
+    }
+
+    blocks.resize(tcnt, 0);
+    for(int i=0; i<tcnt; i++){
+        blocks[i] = temp[i];
+    }
+
+    return tcnt;
+}
 
 
 
@@ -19,23 +38,12 @@ int main() {
     cin >> s1 >> e1;
     cin >> s2 >> e2;
 
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        if(i >= s1-1 && i <= e1-1) continue;
-        temp[cnt] = blocks[i];
-        cnt++;
-    }
-
-    int cnt2 = 0;
-    for(int i=0; i<cnt; i++){
-        if(i >= s2-1 && i <= e2-1) continue;
-        temp2[cnt2] = temp[i];
-        cnt2++;
-    }
+    int cnt1 = removeJenga(n, s1, e1);
+    int cnt2 = removeJenga(cnt1, s2, e2);
 
     cout << cnt2 << '\n';
     for(int i=0; i<cnt2; i++){
-        cout << temp2[i] << endl;
+        cout << blocks[i] << endl;
     }
 
     return 0;
