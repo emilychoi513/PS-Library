c054db1e8d6981aa1129f9ce5f0ad2f9894cac6c
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-02-25 23:44:26 +0900

    Title: 계단 오르기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260225/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 1000
+
+using namespace std;
+
+int n;
+int method[MAX + 1];
+
+int dp(int step){
+    if(step<0) return 0;
+    
+    if(method[step] != -1){
+        return method[step];
+    }else{
+        return method[step] = dp(step-2) + dp(step-3);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    fill(method, method + MAX + 1, -1);
+    method[0] = 0, method[1] = 0;
+    method[2] = 1, method[3] = 1;
+    cout << dp(n);
+
+    return 0;
+}
