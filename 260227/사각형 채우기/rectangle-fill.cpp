ef0cb3679b55c6b446fb07948c17f12a07bfff20
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-02-27 02:09:20 +0900

    Title: 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260227/사각형 채우기/rectangle-fill.cpp
+++ 260227/사각형 채우기/rectangle-fill.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #define MAX 1000
+#define MOP 10007
+
 using namespace std;
 
 int n;
@@ -12,7 +14,7 @@ int main() {
     dp[1] = 1;
     
     for(int i=2; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+        dp[i] = (dp[i-1] + dp[i-2]) % MOP;
     }
 
     cout << dp[n];
