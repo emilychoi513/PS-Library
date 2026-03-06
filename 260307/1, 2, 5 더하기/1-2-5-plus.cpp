ab113174e4368603ed0ddd7a5880dedd8ce01008
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-03-07 01:55:00 +0900

    Title: 1, 2, 5 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260307/1, 2, 5 더하기/1-2-5-plus.cpp
+++ 260307/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -7,32 +7,27 @@ int n;
 int ele[3] = {1, 2, 5};
 long long dp[1000 + 1]; //dp[i] : i를 합으로 하는 경우의 수
 
-void Initial(){
-    for(int i=0; i<3; i++){
-        dp[ele[i]] = 1;
-    }
-}
-
 int main() {
     cin >> n;
 
-    Initial();
+    dp[0] = 1;
 
     for(int i=1; i<=n; i++){
         for(int j=0; j<3; j++){
             if(i >= ele[j]){
                 dp[i] += (dp[i - ele[j]]);
+                dp[i] %= MOP;
             }
         }
     }
-
 
 /*
     for(int i=0; i<=n; i++){
         cout << dp[i] << " ";
     }cout << endl;
 */
-    int ans = dp[n] % MOP;
+
+    long long ans = dp[n] % MOP;
     cout << ans;
 
     return 0;
