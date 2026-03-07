d1d3e59348376d7ac35e6038c5444be045dd127d
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-03-08 01:20:19 +0900

    Title: 최대 동전 거슬러주기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260308/최대 동전 거슬러주기/max-coin-change.cpp
+++ 260308/최대 동전 거슬러주기/max-coin-change.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 int N, M;
 int coin[100];
 int dp[10000 + 1];
+
+void Initial(){
+    for(int i=0; i<=M; i++){
+        dp[i] = INT_MIN;
+    }
+
+    dp[0] = 0;
+}
 
 int main() {
     cin >> N >> M;
@@ -12,10 +21,13 @@ int main() {
         cin >> coin[i];
     }
 
+    Initial();
 
     for(int i=1; i<=M; i++){
         for(int j=0; j<N; j++){
             if(i >= coin[j]){
+                if(dp[i-coin[j]] == INT_MIN) continue;
+
                 dp[i] = max(dp[i], dp[i-coin[j]] + 1);
             }
         }
