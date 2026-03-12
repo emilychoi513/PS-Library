fdbd325d35d20f3a66ac6050a2b5ff77ada2408e
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-03-12 16:19:21 +0900

    Title: 떨어지는 1자 블록, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260312/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, k;
+int grid[100][100];
+
+int main() {
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    k = k-1; // 순서 고려
+
+    int row = 0;
+    for(int i=0; i<n; i++){
+        for(int j=k; j<m+k; j++){
+            if(grid[i][j] == 1){
+                row = i - 1;
+                break;
+            }
+        }
+
+        if(row != 0) break;
+    }
+
+    for(int j=k; j<m+k; j++){
+        grid[row][j] = 1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << grid[i][j] << " ";
+        }cout << endl;
+    }
+
+    return 0;
+}
