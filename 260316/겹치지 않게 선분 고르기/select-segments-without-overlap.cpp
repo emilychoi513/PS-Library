f974ed1e56479fd96f90f91a71e3b16ee1416091
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-03-16 23:49:31 +0900

    Title: 겹치지 않게 선분 고르기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260316/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
+++ 260316/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int n;
 int x1[15], x2[15];
+int visited[15];
 int last;
 int answer;
 int cnt;
 
 void Choose(int end){
-    if(end >= last){
+    bool Exist = false;
+    for(int i=0; i<n; i++){
+        if(visited[i] == 1) continue;
+        if(x1[i] > end) Exist = true;
+    }
+    if(!Exist){
+        //cout << cnt << " : ";
+        //for(int i=0; i<n; i++){
+            //cout << visited[i] << " ";
+        //}cout << endl;
         answer = max(answer, cnt);
         return;
     }
 
 
     for(int i=0; i<n; i++){
-        if(x1[i] > end){
-            cnt++;
-            Choose(x2[i]);
-            cnt--;
-        }
+        if(visited[i] == 1) continue;
+        if(x1[i] <= end) continue;
+        
+        cnt++;
+        visited[i] = 1;
+        Choose(x2[i]);
+        cnt--;
+        visited[i] = 0;
     }
 }
 
