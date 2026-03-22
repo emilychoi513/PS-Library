4b32c873db44931b7fdce3892a771009252793e1
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-03-22 19:30:20 +0900

    Title: 외판원 순회, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260322/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n;
+int A[10][10];
+int visited[10];
+int answer;
+int tmp;
+
+void Move(int curr, int cnt){
+    if(cnt == n-1){
+        //cout << tmp + A[curr][0] << endl;
+        answer = min(answer, tmp + A[curr][0]);
+        return;
+    }
+
+    for(int i=0; i<n; i++){
+        if(visited[i]) continue;
+        if(A[curr][i] == 0) continue;
+
+        tmp += A[curr][i];
+        visited[i] = 1;
+
+        Move(i, cnt + 1);
+
+        tmp -= A[curr][i];
+        visited[i] = 0;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> A[i][j];
+        }
+    }
+
+    answer = INT_MAX;
+    tmp = 0;
+
+    visited[0] = 1;
+    Move(0, 0);
+
+    cout << answer;
+
+    return 0;
+}
