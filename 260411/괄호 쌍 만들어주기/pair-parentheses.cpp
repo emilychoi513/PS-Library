c8e334f288d1d775a3edb98d0a17319aee794aa1
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-04-11 13:12:14 +0900

    Title: 괄호 쌍 만들어주기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260411/괄호 쌍 만들어주기/pair-parentheses.cpp
+++ 260411/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -1,33 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 #include <string>
 using namespace std;
 
-vector<int> Open;
-vector<int> Close;
+vector<pair<int, int>> Open;
+vector<pair<int, int>> Close;
 string str;
 
 int main() {
     cin >> str;
 
-    for(int i=0; i<str.length()-1; i++){
-        if(str[i] == str[i+1]){
-            if(str[i] == '('){
-                Open.push_back(i);
+    int pre_idx = 0, cnt = 1;
+    for(int i=1; i<str.length(); i++){
+        if(str[i] == str[pre_idx]){
+            cnt++;
+        }else{
+            if(cnt < 2){
+                pre_idx = i, cnt = 1;
+                continue;
+            }
+
+            if(str[pre_idx] == '('){
+                Open.push_back(make_pair(pre_idx, cnt));
             }else{
-                Close.push_back(i);
+                Close.push_back(make_pair(pre_idx, cnt));
+            }
+            pre_idx = i, cnt = 1;
+        }
+    }
+    if(str[pre_idx] == '(') Open.push_back(make_pair(pre_idx, cnt));
+    else Close.push_back(make_pair(pre_idx, cnt));
+
+    int ans = 0;
+    for(auto x:Open){
+        for(auto y:Close){
+            if(x.first + (x.second - 1) < y.first){
+                ans += (x.second - 1);
             }
         }
     }
 
-    int answer = 0;
-    for(int x:Open){
-        for(int y:Close){
-            if(x < y) answer++;
-        }
-    }
-
-    cout << answer;
+    cout << ans;
 
     
     return 0;
