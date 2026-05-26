f8e1d982900e8e052fd827dbb20783ade13a020b
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-05-26 15:50:29 +0900

    [Passed] 비율 구하기 (5ms, 1MB)

--- 260526/비율 구하기/find-proportions.cpp
+++ 260526/비율 구하기/find-proportions.cpp
@@ -4,25 +4,24 @@
 using namespace std;
 
 int n;
+string str[10000];
 map<string, int> dict;
 map<string, int>::iterator it;
 
 int main() {
     cin >> n;
 
-    string str;
     for(int i=0; i<n; i++){
-        cin >> str;
-        if(dict.find(str) == dict.end()){
-            dict[str] = 1;
-        }else{
-            dict[str]++;
-        }
+        cin >> str[i];
+    }
+
+    for(int i=0; i<n; i++){
+        dict[str[i]]++;
     }
 
     for(it=dict.begin(); it!=dict.end(); it++){
         cout << it->first << " ";
-        printf("%.4f\n", it->first, (float)it->second/n*100);
+        printf("%.4f\n",(float)it->second/n*100);
     }
     return 0;
 }
