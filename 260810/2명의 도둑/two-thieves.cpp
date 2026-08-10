7fe71423a790a74706b8e7c99126c446264ab2bc
CHOI HYUNJIN <166476995+emilychoi513@users.noreply.github.com>
2026-08-10 14:19:38 +0900

    [Passed] 2명의 도둑 (2ms, 0MB)

--- 260810/2명의 도둑/two-thieves.cpp
+++ 260810/2명의 도둑/two-thieves.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 int n, m, c;
 int weight[10][10];
-vector<int> v;
-int max_value;
-int max_value_1, max_value_2;
+
+int a[10];
+
 int ans;
+int max_val;
 
-pair<int, int> weight_value_sum(){
-    int ws = 0;
-    int vs = 0;
-    for(int num:v){
-        ws += num;
-        vs += pow(num, 2);
-    }
-    return {ws, vs};
-}
-
-void get_max_sum(int x, int y, int right){
-    if(y == right){
-        int weight = weight_value_sum().first;
-        int value = weight_value_sum().second;
+void FindMaxSum(int idx, int weight, int val){
+    if(idx == m){
         if(weight <= c){
-            max_value = max(max_value, value);
+            max_val = max(max_val, val);
         }
         return;
     }
 
-    v.push_back(weight[x][y]);
-    get_max_sum(x, y+1, right); //포함
-    v.pop_back();
+    FindMaxSum(idx+1, weight, val); //미포함
+    FindMaxSum(idx+1, weight + a[idx], val + pow(a[idx], 2)); //포함
 
-    get_max_sum(x, y+1, right); //미포함
 }
 
-void steal(int x1, int y1, int x2, int y2){
-    max_value = 0; 
-    get_max_sum(x1, y1, y1+m);
-    max_value_1 = max_value;
-    max_value = 0;
-    get_max_sum(x2, y2, y2+m);
-    max_value_2 = max_value;
+int FindMax(int x, int y){
+    for(int i=0; i<m; i++){
+        a[i] = weight[x][y+i];
+    }
 
-    //cout << max_value_1 << " " << max_value_2 <<" " ;
-    //cout << max_value_1 + max_value_2 << endl;
-    ans = max(ans, max_value_1 + max_value_2);
+    max_val = 0;
+    FindMaxSum(0, 0, 0);
+    return max_val;
+}
+
+bool Possible(int r1, int c1, int r2, int c2){
+    //경계 초과
+    if(c1+m-1>=n || c2+m-1>=n) return false;
+
+    //겹침
+    if(r1 == r2){
+        if(!((c1+m-1 < c2) || (c2+m-1 < c1))) return false;
+    }
+
+    return true;
 }
 
 int main() {
@@ -60,20 +55,12 @@ int main() {
         }
     }
 
-    //도둑 1,2가 훔칠 영역 골라주기
-    for(int i=0; i<n; i++){
-        for(int j=0; j+m-1<n; j++){
-            for(int a=i; a<n; a++){
-                if(a == i){
-                    for(int b=j+m; b+m-1<n; b++){
-                        //printf("%d %d, %d %d\n", i, j, a, b);
-                        steal(i, j, a, b);
-                    }
-                }
-                else{
-                    for(int b=0; b+m-1<n; b++){
-                        //printf("%d %d, %d %d\n", i, j, a, b);
-                        steal(i, j, a, b);
+    for(int r1=0; r1<n; r1++){
+        for(int c1=0; c1<n; c1++){
+            for(int r2=0; r2<n; r2++){
+                for(int c2=0; c2<n; c2++){
+                    if(Possible(r1, c1, r2, c2)){
+                        ans = max(ans, FindMax(r1, c1) + FindMax(r2, c2));
                     }
                 }
             }
@@ -81,6 +68,5 @@ int main() {
     }
 
     cout << ans;
-
     return 0;
 }
